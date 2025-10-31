6a8303fb10c3b61939e7ac821e36f9b587e6c3ee
gaultierq <gaultierq@users.noreply.github.com>
2025-10-31 21:16:40 +0000

    deploy: 6e3fba9c384a61c01324bde61105093488813913

---
+++ javascripts/site.js
@@ -0,0 +1,29 @@
+import { Application } from "@hotwired/stimulus";
+import TocbotController from "./controllers/tocbot_controller.js";
+import HighlightContoller from "./controllers/highlightjs_controller.js";
+import LolController from "./controllers/lol_controller.js";
+import PaginationController from "./controllers/pagination_controller.js";
+
+console.debug("starting application");
+
+window.onload = function(){
+    const email = document.getElementById('email');
+    if (email) {
+        email.innerText = 'quentin' + '@' + window.location.hostname;
+        email.href = 'mailto:' + email.innerText;
+    }
+
+    const phone = document.getElementById('phone');
+    if (phone) {
+        phone.innerText = '+' + '33' + ' 6' + ' 27' + ' 93' + ' 47' + ' 50' ;
+        phone.href = 'tel:' + phone.innerText;
+    }
+};
+
+window.Stimulus = Application.start();
+window.Stimulus.register("tocbot", TocbotController);
+window.Stimulus.register("highlightjs", HighlightContoller);
+window.Stimulus.register("lol", LolController);
+window.Stimulus.register("pagination", PaginationController);
+
+
